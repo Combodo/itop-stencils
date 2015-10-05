12004bb6e686bf7568a6e397100c81f74ee800f9
Romain Quetiez <romain.quetiez@combodo.com>
2015-10-05 09:47:24 +0000

    1.0.6 ... oooops 1.0.5 was declared as "1.0.5 " (notice the white space at the end)
    SVN:trunk[4791]

--- module.itop-stencils.php
+++ module.itop-stencils.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'itop-stencils/1.0.5 ',
+	'itop-stencils/1.0.6',
 	array(
 		// Identification
 		//
